283f66c4f8cd7adf49dbea0c6f97d5481e450a5b
molt0bene <alisar3@yandex.ru>
2022-10-04 23:18:45 +0300

    describe in spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -20,40 +20,42 @@ RSpec.describe Game, type: :model do
   end
 
   # Группа тестов на работу фабрики создания новых игр
-  context 'Game Factory' do
-    it 'Game.create_game! new correct game' do
-      # Генерим 60 вопросов с 4х запасом по полю level, чтобы проверить работу
-      # RANDOM при создании игры.
-      generate_questions(60)
+  describe 'Game Factory' do
+    describe '.create_game!' do
+      it 'creates new correct game' do
+        # Генерим 60 вопросов с 4х запасом по полю level, чтобы проверить работу
+        # RANDOM при создании игры.
+        generate_questions(60)
 
-      game = nil
+        game = nil
 
-      # Создaли игру, обернули в блок, на который накладываем проверки
-      expect {
-        game = Game.create_game_for_user!(user)
-        # Проверка: Game.count изменился на 1 (создали в базе 1 игру)
-      }.to change(Game, :count).by(1).and(
-        # GameQuestion.count +15
-        change(GameQuestion, :count).by(15).and(
-          # Game.count не должен измениться
-          change(Question, :count).by(0)
+        # Создaли игру, обернули в блок, на который накладываем проверки
+        expect {
+          game = Game.create_game_for_user!(user)
+          # Проверка: Game.count изменился на 1 (создали в базе 1 игру)
+        }.to change(Game, :count).by(1).and(
+          # GameQuestion.count +15
+          change(GameQuestion, :count).by(15).and(
+            # Game.count не должен измениться
+            change(Question, :count).by(0)
+          )
         )
-      )
 
-      # Проверяем статус и поля
-      expect(game.user).to eq(user)
-      expect(game.status).to eq(:in_progress)
+        # Проверяем статус и поля
+        expect(game.user).to eq(user)
+        expect(game.status).to eq(:in_progress)
 
-      # Проверяем корректность массива игровых вопросов
-      expect(game.game_questions.size).to eq(15)
-      expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+        # Проверяем корректность массива игровых вопросов
+        expect(game.game_questions.size).to eq(15)
+        expect(game.game_questions.map(&:level)).to eq (0..14).to_a
+      end
     end
   end
 
   # Тесты на основную игровую логику
-  context 'game mechanics' do
+  describe 'game mechanics' do
     # Правильный ответ должен продолжать игру
-    it 'answer correct continues game' do
+    it 'correct answer continues game' do
       # Текущий уровень игры и статус
       level = game_w_questions.current_level
       q = game_w_questions.current_game_question
